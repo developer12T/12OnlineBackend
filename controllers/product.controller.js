11d2c259e1979ee8450daa36aa5cc19494b09c97
Nopparat Munsuwan <nopparat.mun@onetwotrading.co.th>
2025-12-11 16:56:35 +0700

    update product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,28 +3,13 @@ const axios = require('axios')
 const productModel = require('../model/product')
 const { getModelsByChannel } = require('../authen/middleware/channel')
 
-
 exports.sysnceProductBento = async (req, res) => {
   try {
     const { action, langs, limit, date_created_start, date_created_end } =
       req.query
     const channel = req.headers['x-channel']
     const { Product } = getModelsByChannel(channel, res, productModel)
-    console.log(channel)
-
     const token = await getAccessToken()
-    // console.log(
-    //   `${process.env.BENTO_STORE_URL}/productlist?store_id=${process.env.CLIENT_ID}`,
-    //   {
-    //     headers: {
-    //       Authorization: `Bearer ${token}`
-    //     },
-    //     params: {
-    //       limit
-    //     }
-    //   }
-    // )
-
     const response = await axios.get(
       `${process.env.BENTO_STORE_URL}/productlist?store_id=${process.env.CLIENT_ID}`,
       {
@@ -38,33 +23,78 @@ exports.sysnceProductBento = async (req, res) => {
         }
       }
     )
-
     const products = response.data?.data?.product_list || []
-
     // -----------------------------
     // INSERT / UPDATE TO MONGO
     // -----------------------------
-    for (const item of products) {
-      await Product.updateOne(
-        { product_id: item.product_id }, // filter
-        {
-          $set: {
-            product_id: item.product_id,
-            store_id: item.store_id,
-            name: item.name,
-            type: item.type,
-            price: item.price,
-            selling_price: item.selling_price,
-            weight: item.weight,
-            is_sold_out: item.is_sold_out,
-            active: item.active,
-            thumb: item.thumb,
-            image: item.image,
-            product_variant: item.product_variant || []
-          }
-        },
-        { upsert: true }
-      )
+    // for (const items of products) {
+    //   for (const item of items.product_variant) {
+    //     await Product.updateOne(
+    //       { product_variant_id: item.product_variant_id }, // filter
+    //       {
+    //         $set: {
+    //           product_variant_id: item.product_variant_id,
+    //           product_id: item.product_id,
+    //           product_name: item.product_name,
+    //           sku: item.sku,
+    //           name: item.name,
+    //           group_id: items.product_id,
+    //           group: items.name,
+    //           store_id: item.store_id,
+    //           type: item.type,
+    //           price: item.price,
+    //           stock: item.stock,
+    //           weight: items.weight,
+    //           selling_price: items.selling_price,
+    //           is_sold_out: items.is_sold_out,
+    //           active: items.active,
+    //           thumb: items.thumb,
+    //           image: items.image,
+    //           product_thumb: items.product_thumb,
+    //           product_image: items.product_image
+    //           //   product_variant: item.product_variant || []
+    //         }
+    //       },
+    //       { upsert: true }
+    //     )
+    //   }
+    // }
+    for (const product of products) {
+      const variants = product.product_variant || []
+
+      for (const item of variants) {
+        await Product.updateOne(
+          { product_variant_id: item.product_variant_id },
+          {
+            $set: {
+              // product-level
+              product_id: product.id,
+              group_id: product.product_id,
+              group: product.name,
+              weight: product.weight,
+              selling_price: product.selling_price,
+              store_id: item.store_id,
+              is_sold_out: product.is_sold_out,
+              active: product.active,
+              thumb: product.thumb,
+              image: product.image,
+              product_thumb: product.product_thumb,
+              product_image: product.product_image,
+              // variant-level
+              product_variant_id: item.product_variant_id,
+              product_id: item.product_id,
+              product_name: item.product_name,
+              sku: item.sku,
+              name: item.name,
+              type: item.type,
+              price: item.price,
+              seq: item.seq,
+              stock: item.stock
+            }
+          },
+          { upsert: true }
+        )
+      }
     }
 
     // -------------------------------------------
--- model/product.js
+++ model/product.js
@@ -1,31 +1,41 @@
 const mongoose = require('mongoose')
 
-const listProductVariantSchema = new mongoose.Schema({
-  product_variant_id: { type: Number },
-  product_id: { type: Number },
-  product_name: { type: String },
-  name: { type: String },
-  sku: { type: String },
-  barcode: { type: String },
-  price: { type: Number },
-  seq: { type: Number },
-  updated_at: { type: String }
-})
+// const listProductVariantSchema = new mongoose.Schema({
+//   product_variant_id: { type: Number },
+//   product_id: { type: Number },
+
+//   name: { type: String },
+
+//   updated_at: { type: String }
+// })
 
 const productSchema = new mongoose.Schema(
   {
-    product_id: { type: Number, index: true },   // แนะนำให้มี index
+    product_variant_id: { type: Number },
+    product_id: { type: Number, index: true }, // แนะนำให้มี index
+    group_id: { type: Number, index: true }, // แนะนำให้มี index
+    product_name: { type: String },
     store_id: { type: Number, index: true },
-    name: { type: String },
+    group: { type: String },
+    sku: { type: String },
     type: { type: Number },
     price: { type: Number },
+    stock: { type: Number },
+    real_stock: { type: Number },
     selling_price: { type: Number },
     weight: { type: Number },
     is_sold_out: { type: Boolean },
     active: { type: Boolean },
     thumb: { type: String },
+    barcode_type: { type: Number },
+    barcode: { type: String },
     image: { type: String },
-    product_variant: [listProductVariantSchema]
+    barcode: { type: String },
+    product_thumb: { type: String },
+    product_image: { type: String },
+    // price: { type: Number },
+    seq: { type: Number }
+    // product_variant: [listProductVariantSchema]
   },
   {
     timestamps: true
